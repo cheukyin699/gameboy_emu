3a66f6f73adec4ad2d059f2c36d0d2d501d5b72c
Cheuk Yin Ng <cheukyin699@yahoo.com>
2015-09-21 11:46:31 -0700

    Added licensing

--- src/gb_memory.h
+++ src/gb_memory.h
@@ -1,3 +1,18 @@
+/* This file is part of gameboy_emu.
+ *
+ * gameboy_emu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * gameboy_emu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with gameboy_emu.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef GB_MEMORY_H_
 #define GB_MEMORY_H_
 
